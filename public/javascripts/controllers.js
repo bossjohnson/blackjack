983f980c3febb36f83bc44ef035ae22682e95ab1
Josh Newsom <newsommeister@gmail.com>
2016-08-01 22:22:42 -0600

    Calculate and display value of player hand

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -2,13 +2,19 @@ app.controller('blackjackController', blackjackController);
 
 function blackjackController($scope, cardService) {
 
-    $scope.view = {};
-
     $scope.buildDeck = cardService.buildDeck;
-    $scope.test = function() {
-        var deck = cardService.buildDeck();
-        deck.shuffle();
-        $scope.view.deck = deck;
+
+    $scope.view = {
+        deck: $scope.buildDeck(),
+        playerHand: {}
+    };
+
+    $scope.view.deck.shuffle();
+
+    $scope.deal = function() {
+        var deck = $scope.view.deck
+        $scope.view.playerHand.cards = [deck.cards.pop(), deck.cards.pop()];
+        $scope.view.playerHand.value = handValue($scope.view.playerHand);
     }
 
     $scope.collect = function(rank) {
@@ -25,5 +31,35 @@ function blackjackController($scope, cardService) {
         return array;
     };
 
+
 }
 blackjackController.$inject = ['$scope', 'cardService'];
+
+// ================
+// Helper Functions
+// ================
+function handValue(hand) {
+    console.log(hand.cards);
+    return hand.cards.reduce(sumCards);
+}
+
+function sumCards(a, b) {
+
+    if (typeof a.rank === 'number') { // handle non-face cards
+        a.value = a.rank;
+    } else if (a.rank === 'J' || a.rank === 'Q' || a.rank === 'K') { // handle face cards
+        a.value = 10;
+    } else { // handle aces
+        a.value = 11;
+    }
+
+    if (typeof b.rank === 'number') { // handle non-face cards
+        b.value = b.rank;
+    } else if (b.rank === 'J' || b.rank === 'Q' || b.rank === 'K') { // handle face cards
+        b.value = 10;
+    } else { // handle aces
+        b.value = 11;
+    }
+
+    return a.value + b.value;
+}
